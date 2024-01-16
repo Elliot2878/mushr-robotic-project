350c6510db9638360ee1f7338104e3cca6a18c7f
Zhenyuan Liang <74114408+Elliot2878@users.noreply.github.com>
2024-01-15 20:45:50 -0500

    Update part2.py

--- src/part2.py
+++ src/part2.py
@@ -11,8 +11,8 @@ from ackermann_msgs.msg import AckermannDriveStamped
 
 
 class cybertruck2:
-    latest_img = None
-    ackermann_msg_id = 0
+latest_img = None
+ackermann_msg_id = 0
     # dir1='~/catkin_ws/src/cybertruck2/src' 
     # os.chdir(dir1)
     def __init__(self):
@@ -20,308 +20,243 @@ class cybertruck2:
         #global cur_vel
         # cur_vel = 0
         #global a
-        self.a = [0.0, 0.0, 0.0]
-        self.sub = rospy.Subscriber('/csi_cam_0/image_raw', Image, self.im_callback)
-        self.if_flag = False
-        self.x = 0.0
-        self.flag_reached = False
-        # self.cur_vel = 0
-        # self.a = [0.0, 0.0, 0.0]
-        
-        # send
-        self.pub = rospy.Publisher(
-            rospy.get_param(
+self.a = [0.0, 0.0, 0.0]
+self.sub = rospy.Subscriber('/csi_cam_0/image_raw', Image, self.im_callback)
+self.if_flag = False
+self.x = 0.0
+self.flag_reached = False
+
+self.pub = rospy.Publisher(
+rospy.get_param(
                 "~ctrl_topic",
-                default = "mux/ackermann_cmd_mux/input/navigation"
+default = "mux/ackermann_cmd_mux/input/navigation"
             ),
-            AckermannDriveStamped,
-            queue_size = 10
+AckermannDriveStamped,
+queue_size = 10
         )
 
-        self.im_pub = rospy.Publisher("/debug_image", Image)
-        self.timer = rospy.Timer(rospy.Duration(1.0/50), self.pub_callback)
+self.im_pub = rospy.Publisher("/debug_image", Image)
+self.timer = rospy.Timer(rospy.Duration(1.0/50), self.pub_callback)
 
 
 
     def im_callback(self, data):
-        # global latest_img
-        latest_img = data
-        rospy.loginfo(rospy.get_caller_id() + 'Received a new image.')
+
+latest_img = data
+rospy.loginfo(rospy.get_caller_id() + 'Received a new image.')
         
-        bridge = CvBridge()
-        cv_image = bridge.imgmsg_to_cv2(latest_img, desired_encoding = 'passthrough')
-        self.if_flag, self.x, self.flag_reached = self.detect_flag(cv_image)
+bridge = CvBridge()
+cv_image = bridge.imgmsg_to_cv2(latest_img, desired_encoding = 'passthrough')
+self.if_flag, self.x, self.flag_reached = self.detect_flag(cv_image)
         
         '''
-        img = cv_image[:,:,::-1]
-       # cv2.imwrite('yellow_bgr.png', img)
+ img = cv_image[:,:,::-1]
+ 
 
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+ img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-        #pixel_min = np.array([pixel[0] - 80, pixel[1] - 80, pixel[2] - 80])
-        #pixel_max = np.array([pixel[0] + 80, pixel[1] + 80, pixel[2] + 80])
 
-        pixel_min = np.array([90, 70, 90])
-        pixel_max = np.array([126, 225, 225])
-        # print(np.shape(img))
-        mask = cv2.inRange(img, pixel_min, pixel_max)
-        ret, thresh = cv2.threshold(mask, 50, 255, cv2.THRESH_BINARY)
-        _, contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        if len(contours) > 0:
-            max_contour = max(contours, key = len)
-            flag = cv2.minAreaRect(max_contour)[0]
-            x = int(flag[0])
-            y = int(flag[1])
-            pixel = img[y, x]
-            output = str(pixel[0]) + ','+  str(pixel[1]) + ',' + str(pixel[2])
-            print('output for noise2:', output)
-            
-            box = cv2.boxPoints(cv2.minAreaRect(max_contour))
-            box = np.int0(box)
-            img_bgr = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
-            # frame_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
-            draw_box = cv2.drawContours(img_bgr,[box],0,255,2)
-            img_to_pub = bridge.cv2_to_imgmsg(draw_box)
-            self.im_pub.publish(img_to_pub)
-        '''
+ pixel_min = np.array([90, 70, 90])
+ pixel_max = np.array([126, 225, 225])
+
+ mask = cv2.inRange(img, pixel_min, pixel_max)
+ ret, thresh = cv2.threshold(mask, 50, 255, cv2.THRESH_BINARY)
+ _, contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+ if len(contours) > 0:
+ max_contour = max(contours, key = len)
+ flag = cv2.minAreaRect(max_contour)[0]
+ x = int(flag[0])
+ y = int(flag[1])
+ pixel = img[y, x]
+ output = str(pixel[0]) + ','+ str(pixel[1]) + ',' + str(pixel[2])
+ print('output for noise2:', output)
+ 
+ box = cv2.boxPoints(cv2.minAreaRect(max_contour))
+ box = np.int0(box)
+ img_bgr = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
+
+ draw_box = cv2.drawContours(img_bgr,[box],0,255,2)
+ img_to_pub = bridge.cv2_to_imgmsg(draw_box)
+ self.im_pub.publish(img_to_pub)
+ '''
     def pub_callback(self, event):
         #global cur_vel
         #global a
         #cur_vel = cur_vel + a[1] * (1.0/10)
        # print('check self.if_flag: ', self.if_flag)
         #steer = self.search_env()
-        self.a = self.search_env()
-        ctrl = [0.0, 0.0]
+self.a = self.search_env()
+ctrl = [0.0, 0.0]
         print("check self.a: ", self.a[1])
-        ctrl[0] = float(self.a[1])
-        ctrl[1] = float(self.a[0])
+ctrl[0] = float(self.a[1])
+ctrl[1] = float(self.a[0])
         # r = rospy.Rate(1.0/50)
-        self.publish_ctrl(self.pub, ctrl)
+self.publish_ctrl(self.pub, ctrl)
 
 
     def detect_flag(self, img):
-        if_flag = False
-        x = 0
-        flag_reached = False
-        img = img[:,:,::-1]
-        cv2.imwrite('yellow_bgr002.png', img)
+if_flag = False
+x = 0
+flag_reached = False
+img = img[:,:,::-1]
+cv2.imwrite('yellow_bgr002.png', img)
         
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         
         '''
-        pixel_file = open("flag2.txt", "r")
-        pixel_list = pixel_file.read().split(",")
-        pixel = list(map(int, pixel_list))
-        print(pixel)
-        output = str(pixel[0]) + ','+  str(pixel[1]) + ',' + str(pixel[2])
-        print('output in txt file:', output)
-        #pixel_min = np.array([pixel[0] - 80, pixel[1] - 80, pixel[2] - 80])
-        #pixel_max = np.array([pixel[0] + 80, pixel[1] + 80, pixel[2] + 80])
-        '''
+ pixel_file = open("flag2.txt", "r")
+ pixel_list = pixel_file.read().split(",")
+ pixel = list(map(int, pixel_list))
+ print(pixel)
+ output = str(pixel[0]) + ','+ str(pixel[1]) + ',' + str(pixel[2])
+ print('output in txt file:', output)
 
-        pixel_min = np.array([90, 70, 90])
-        pixel_max = np.array([126, 225, 225])
+ '''
+
+pixel_min = np.array([90, 70, 90])
+pixel_max = np.array([126, 225, 225])
         
         # 2 is for yellow
-        pixel_min2 = np.array([20, 40, 60])
-        pixel_max2 = np.array([55, 255, 255])
-        # print(np.shape(img))
-        mask = cv2.inRange(img, pixel_min, pixel_max)
-        mask2 = cv2.inRange(img, pixel_min2, pixel_max2)
+pixel_min2 = np.array([20, 40, 60])
+pixel_max2 = np.array([55, 255, 255])
+
+mask = cv2.inRange(img, pixel_min, pixel_max)
+mask2 = cv2.inRange(img, pixel_min2, pixel_max2)
         print('Unique Mask: ', np.unique(mask))
         print('Unique Mask2: ', np.unique(mask2))
-        count = np.sum(np.nonzero(mask))
-        count = np.sum(mask)
+count = np.sum(np.nonzero(mask))
+count = np.sum(mask)
     
-        count2 = np.sum(mask2)
+count2 = np.sum(mask2)
         if count == 0 and count2 == 0:
-            if_flag = False
+if_flag = False
         else:
-            if_flag = True
+if_flag = True
 
 
         if if_flag == False:
             return [False, -1, False]
 
-        mask = cv2.bitwise_or(mask, mask2)
-        ret, thresh = cv2.threshold(mask, 50, 255, cv2.THRESH_BINARY)
-        _, contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        if len(contours) > 0: 
-            max_contour = max(contours, key = len)
-            flag = cv2.minAreaRect(max_contour)[0]
+mask = cv2.bitwise_or(mask, mask2)
+ret, thresh = cv2.threshold(mask, 50, 255, cv2.THRESH_BINARY)
+_, contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        if len(contours) > 0:
+max_contour = max(contours, key = len)
+flag = cv2.minAreaRect(max_contour)[0]
             print("check x:", flag[0])
-            x = flag[0]
+x = flag[0]
         else:
-            x = -1
+x = -1
     
         
-        # commenting testing code here
-        #cv2.imwrite('binary.png',mask)
-        #box = cv2.boxPoints(cv2.minAreaRect(max_contour))
-        #box = np.int0(box)
-        #img_bgr = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
-        #draw_box = cv2.drawContours(img_bgr,[box],0,255,2)
-        #cv2.imwrite("draw_box.png", draw_box)
-        
-        
-        # output = cv2.bitwise_and(img_bgr, img_bgr, mask=mask)
-        # cv2.drawContours(output, contours, -1, 255, 3)
-
-        # # find the biggest countour (c) by the area
-        # c = max(contours, key = cv2.contourArea)
-        #print("biggest c:", c)
-        #x,y,w,h = cv2.boundingRect(c)
-
-        # draw the biggest contour (c) in green
-        #cv2.rectangle(output,(x,y),(x+w,y+h),(0,255,0),2)
-
-        # show the images
-        #cv2.imwrite("draw_box.png", np.hstack([img_bgr, output]))
 
         
         
         if(len(contours) > 0):
-            box = cv2.boxPoints(cv2.minAreaRect(max_contour))
-            box_width = box[0][0] - box[1][0]
-            box_height = box[0][1] - box[3][1]
+box = cv2.boxPoints(cv2.minAreaRect(max_contour))
+box_width = box[0][0] - box[1][0]
+box_height = box[0][1] - box[3][1]
             
-            # box_width, box_height = cv2.minAreaRect(contours[0])[1]
-            print("box_width: ", box_width)
-            print("box_height: ", box_height)
+     
             
             # 5 or 5 is probably better on the ground
             if(box_height < 5 or box_width < 5):
-                if_flag = False
+if_flag = False
                 print("You removed the noise: ", if_flag)
             
             #if(box_height > 250 or box_width > 506):
-            #    print("IT IS TRUE NOW!!!!")
-            #    flag_reached = True
+            # print("IT IS TRUE NOW!!!!")
+            # flag_reached = True
             #else:
-            #    flag_reached = False
+            # flag_reached = False
         
-        output = [False, 0, False]
-        output[0] = if_flag
-        output[1] = x
-        output[2] = flag_reached
-        #self.if_flag = output[0]
-        #self.x = output[1]
-        #self.flag_reached = output[2]
-        # print('test_if_flag', output[0])
-        # print('test_flag_reached', output[2])
+output = [False, 0, False]
+output[0] = if_flag
+output[1] = x
+output[2] = flag_reached
+
         return output
 
     def search_env(self):
         if(self.if_flag == False):
             #a = [0.002, 0.05, 0.0]
-            a = [0.002, 0.05, 0.0]
+a = [0.002, 0.05, 0.0]
             return a
-        #elif(self.flag_reached == True):
-          #  print("The flag has reached")
-        #    a = [0.0, 0.0, 0.0]
-        #    return a
 
-        # a = [steering, gas, brake(useless here)]
-        a = [0.0, 0.05, 0.0]
-        center = 640
+
+
+a = [0.0, 0.05, 0.0]
+center = 640
         # best kp = 0.05
-        kp = 0.0006
+kp = 0.0006
         # best kd = 0.03
-        kd = 0.0003
-        goal = 0
-        goal_vel = 0
-        cur = center - self.x
-        max_a = 1
+kd = 0.0003
+goal = 0
+goal_vel = 0
+cur = center - self.x
+max_a = 1
 
-        # if(goal - cur) > 1:
-        #    print("let go")
-        #    a[1] = 0
 
-        #print("error: ", goal - cur)
-        des_a = kp*(goal-cur) 
-        #kd*(goal_vel-cur_vel)
-        cl_a = np.clip(des_a, -max_a, max_a)
-        a[0] = cl_a
-        #print("print a when True: ", a[0])
+
+des_a = kp*(goal-cur) 
+
+cl_a = np.clip(des_a, -max_a, max_a)
+a[0] = cl_a
+        
         
         # since the camera is up side down, we return -a value
-        a[0] = -a[0] + 0.07
+a[0] = -a[0] + 0.07
          
-        return a           
+        return a
         
 
 
     def publish_ctrl(self, rp_ctrls, ctrl):
         assert len(ctrl) == 2
-        ctrlmsg = AckermannDriveStamped()
-        ctrlmsg.header.stamp = rospy.Time.now()
+ctrlmsg = AckermannDriveStamped()
+ctrlmsg.header.stamp = rospy.Time.now()
         # ctrlmsg.header.seq = ackermann_msg_id
-        ctrlmsg.drive.speed = ctrl[0]
-        ctrlmsg.drive.steering_angle = ctrl[1]
-        rp_ctrls.publish(ctrlmsg)    
-        rospy.loginfo("publishing ctrl")
+ctrlmsg.drive.speed = ctrl[0]
+ctrlmsg.drive.steering_angle = ctrl[1]
+rp_ctrls.publish(ctrlmsg)    
+rospy.loginfo("publishing ctrl")
 
 
 def register_flag(img):
-    # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # ret, thresh = cv2.threshold(img, 50, 255, cv2.THRESH_BINARY)
-    # contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+   
     
     
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    pixel_min = np.array([15, 50, 70])
-    pixel_max = np.array([55, 255, 255])
-    mask = cv2.inRange(img, pixel_min, pixel_max)
-    cv2.imwrite("take_picture_blue.jpg", mask)
-    _, contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    max_contour = max(contours, key = len)
-    flag = cv2.minAreaRect(max_contour)[0]
-    x = int(flag[0])
-    y = int(flag[1])
-    #print(x)
-    #print(y)
-    #img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    #img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
-    # pixel = [img[y, x, 0], img[y, x, 1], img[y, x, 2]]
-    pixel = img[y, x]
+img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+pixel_min = np.array([15, 50, 70])
+pixel_max = np.array([55, 255, 255])
+mask = cv2.inRange(img, pixel_min, pixel_max)
+cv2.imwrite("take_picture_blue.jpg", mask)
+_, contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+max_contour = max(contours, key = len)
+flag = cv2.minAreaRect(max_contour)[0]
+x = int(flag[0])
+y = int(flag[1])
+
+pixel = img[y, x]
 
     
-    box = cv2.boxPoints(cv2.minAreaRect(max_contour))
-    box = np.int0(box)
-    img_bgr = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
-    draw_box = cv2.drawContours(img_bgr,[box],0,255,2)
-    cv2.imwrite("input_box_blue.png", draw_box)
-    # box = cv2.boxPoints(cv2.minAreaRect(contours[0]))
-    # box = np.int0(box)
-    # draw_box = cv2.drawContours(img,[box],0,255,1)
-    # cv2.imwrite('register.png', draw_box)
-    f = open('flag2.txt', 'w')
-    output = str(pixel[0]) + ','+  str(pixel[1]) + ',' + str(pixel[2])
+box = cv2.boxPoints(cv2.minAreaRect(max_contour))
+box = np.int0(box)
+img_bgr = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
+draw_box = cv2.drawContours(img_bgr,[box],0,255,2)
+cv2.imwrite("input_box_blue.png", draw_box)
+
+f = open('flag2.txt', 'w')
+output = str(pixel[0]) + ','+ str(pixel[1]) + ',' + str(pixel[2])
     print('output', output)
-    f.write(output)
+f.write(output)
     return pixel
 
 if __name__ == '__main__':
-    rospy.init_node("drive")
-
-    #test_image = cv2.imread('yellow_bgr002.png')
-
-    #print("Shape of test_image", np.shape(test_image))
-
-    #register_flag(test_image)
-
-    node = cybertruck2()
-    rospy.spin()
-    # image_message = latest_img
-    # bridge = CvBridge()
-    # cv_image = bridge.imgmsg_to_cv2(image_message, desired_encoding = 'passthrough')
-    
+rospy.init_node("drive")
 
 
-    # print(detect_flag(cv_image))
-    # listener()
-    
-    #while not rospy.is_shutdown():
-        # steer = search_env(self.if_flag, self.x, cur_vel)
-    #    publish_ctrl(rp_ctrls, ctrl)
-    #    r.sleep()
+
+node = cybertruck2()
+rospy.spin()
+
